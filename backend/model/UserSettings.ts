d73d3b693551fd9d41ae39e103579aea28e26016
chenusimon <49160235@est.ort.edu.ar>
2026-08-19 19:23:45 -0300

    minor: agregue los modelos de la db al backend

---
+++ backend/model/UserSettings.ts
@@ -0,0 +1,8 @@
+export interface UserSettings {
+  id: string;
+  user_id: string;
+  dark_mode: boolean;
+  save_history: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
---
+++ backend/model/index.ts
@@ -0,0 +1,4 @@
+export type { User } from "./User";
+export type { UserSettings } from "./UserSettings";
+export type { Chat } from "./Chat";
+export type { Message, MessageRole } from "./Message";
